ddb675898845177a7f81baf9eb1174b579e24b23
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-29 12:36:11 +0200

    first commit

---
+++ src/main/java/zw/co/afrosoft/RestfulApiDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApiDiscoveryServiceApplication.class, args);
+    }
+
+}
